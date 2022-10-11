5651bed0e043a7c61a41c813b17e8dfbf58120a5
HaneenAlotaibi <haneen.alotaibi@ccse.ku.edu.kw>
2022-10-11 20:14:55 +0300

    finish

--- pokemon/admin.py
+++ pokemon/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Pokemon 
 
 # Register your models here.
+@admin.register(Pokemon)
+class PokemonAdmin(admin.ModelAdmin):
+    list_display = ("id", "name","hp","active")
+    list_filter=("active",)
+    list_display_links = ('id', 'name')
+    fieldsets = (
+      ('Localizations', {
+          'fields': ('name_fr','name_ar','name_jp')
+      }),
+      
+   )
--- pokemon/models.py
+++ pokemon/models.py
@@ -1,3 +1,25 @@
+from email.policy import default
+from enum import auto
+from random import choices
+from secrets import choice
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
 # Create your models here.
+class Pokemon(models.Model):
+    class PokemonType(models.TextChoices):
+        WATER = 'WA'
+        GRASS = 'GR'
+        GHOST = 'GH'
+        STEEL = 'ST'
+        FAIRY = 'FA'
+    name = models.CharField(max_length=30)
+    type=models.CharField(max_length=10,choices=PokemonType.choices,default='FA')
+    hp=models.PositiveIntegerField() 
+    active=models.BooleanField(default=True) 
+    name_fr= models.CharField(max_length=30,default="")
+    name_ar=models.CharField(max_length=30,default="")
+    name_jp=models.CharField(max_length=30,default="")
+    created_at= models.DateTimeField(auto_now_add=True)
+    modified_at=models.DateTimeField(auto_now=True)
+     
